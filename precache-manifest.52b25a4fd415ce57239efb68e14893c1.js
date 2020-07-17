e317d50eb9a4530571122b6d57d4493ec7a14422
linhnguyengts <linhnguyengts@gmail.com>
2020-07-17 10:58:53 +0700

    Updates

---
+++ precache-manifest.52b25a4fd415ce57239efb68e14893c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee1a870036f52bb83777e17af6cbb60",
+    "url": "/barcode-generator/index.html"
+  },
+  {
+    "revision": "96f7cafabd162c78241f",
+    "url": "/barcode-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f494ed217ecfcfc6631",
+    "url": "/barcode-generator/static/js/2.b7b3a749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/barcode-generator/static/js/2.b7b3a749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f7cafabd162c78241f",
+    "url": "/barcode-generator/static/js/main.06ccbf9a.chunk.js"
+  },
+  {
+    "revision": "93f812b0c7e71de56667",
+    "url": "/barcode-generator/static/js/runtime-main.87bd1ccf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/barcode-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
